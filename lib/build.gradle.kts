bdc6009d8f3c7bd73e0eb277e2ce68a609a1b0d5
Eric Vantillard <eric.vantillard@evaxion.fr>
2023-03-07 20:04:08 +0100

    build: use a versions catalog

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -67,10 +67,10 @@ repositories {
 
 dependencies {
     // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
+    api(libs.commons.math3)
 
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:31.1-jre")
+    implementation(libs.guava)
 }
 
 testing {
@@ -78,7 +78,7 @@ testing {
         // Configure the built-in test suite
         val test by getting(JvmTestSuite::class) {
             // Use JUnit Jupiter test framework
-            useJUnitJupiter("5.9.1")
+            useJUnitJupiter(libs.versions.junit.get())
         }
     }
 }
